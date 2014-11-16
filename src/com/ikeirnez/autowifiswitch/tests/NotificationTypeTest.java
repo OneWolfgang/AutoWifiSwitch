21018af50dc3b270b7d26c7b67431b3fef801e60
Keir Nellyer <keir@nellyer.co.uk>
2014-11-16 11:28:37 +0000

    Today I wrote a unit test, and it works :)

---
+++ src/com/ikeirnez/autowifiswitch/tests/NotificationTypeTest.java
@@ -0,0 +1,29 @@
+package com.ikeirnez.autowifiswitch.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityTestCase;
+import android.test.ActivityUnitTestCase;
+import com.ikeirnez.autowifiswitch.Main;
+import com.ikeirnez.autowifiswitch.R;
+import com.ikeirnez.autowifiswitch.enums.NotificationType;
+
+/**
+ * Created by iKeirNez on 14/11/2014.
+ */
+public class NotificationTypeTest extends ActivityInstrumentationTestCase2<Main> {
+
+    public NotificationTypeTest() {
+        super(Main.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    // Check's that the default notification type (defined manually in an xml file) actually exists
+    public void testNotificationType() throws Exception {
+        String defaultNotificationType = getActivity().getResources().getString(R.string.default_notification_type);
+        assertNotNull(NotificationType.valueOf(defaultNotificationType)); // this will never actually be null but rather throw an exception, either way we are alerted to an issue
+    }
+}
